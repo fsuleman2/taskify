0b116c60c7dc2b170a21e8b1673f6c08f3b231a8
Syed Suleman <sydsulmn7@gmail.com>
2024-07-07 18:52:44 +0530

    setup the repo and created basic home page

---
+++ src/components/InputField.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../components/styles.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({ todo, setTodo }: Props) => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
