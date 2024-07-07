f274e6bed96cf30f44214bccdd5a0eac44a9abd7
Syed Suleman <sydsulmn7@gmail.com>
2024-07-07 20:03:59 +0530

    get rid of blur when press enter

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../components/styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
-  handleAdd:(e: React.FormEvent)=> void;
+  handleAdd: (e: React.FormEvent) => void;
 }
-
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd}) => {
+/* one bug we got actually whenever you type something in text box and press enter it's still showing up the blur effect 
+so to removed that we are using UseRef hook, which is basically like document.getElementByID() kind of thing in java scrirpt.
+so basically we are taking the ref(id) of the input box and removing the blur effect.
+*/
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
+      ref={inputRef}
         type="input"
         placeholder="Enter a task"
         className="input__box"
