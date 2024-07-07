c640a8fca9fcf707b148e940f839b0b8eb57f90c
Syed Suleman <sydsulmn7@gmail.com>
2024-07-07 19:29:39 +0530

    created model to store the todo details

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from "./models/model";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string | number>("");
+  //state to hold the todo typed in textbox
+  const [todo, setTodo] = useState<string>("");
+  //state to hold all the todos along with details
+  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField  todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,10 +6,17 @@ interface Props {
   setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+console.log(todo, "todo");
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input__box" />
+      <input
+        type="input"
+        placeholder="Enter a task"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button className="input_submit" type="submit">
         Go
       </button>
